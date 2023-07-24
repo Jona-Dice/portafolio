39d1ee29b35dbe3493bfb22bc9125afa39f9d1cc
jonadice <heyjonadice@gmail.com>
2023-07-24 15:03:19 -0600

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,17 +9,33 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+  private cargarInfo(){
     //console.log('Servicio de infoPagina listo');
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
+      //console.log(resp);
+
+    });
+   }
+  private cargarEquipo(){
+
+    //console.log('Servicio de infoPagina listo');
+    this.http.get('https://angular-html-f6c55-default-rtdb.firebaseio.com/Equipo.json')
+    .subscribe( (resp: any) => {
+
+      this.equipo = resp;
       console.log(resp);
 
     });
-
-   }
+  }
 }
